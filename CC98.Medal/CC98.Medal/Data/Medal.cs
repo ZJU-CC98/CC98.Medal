f7b5900b7a25ffeef9b76d058de4b1765e60239b
Iris Sakura <sgjsakura@hotmail.com>
2020-06-28 20:53:55 +0800

    Update

---
+++ CC98.Medal/CC98.Medal/Data/Medal.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CC98.Medal.Data
+{
+	/// <summary>
+	/// 表示一个勋章。
+	/// </summary>
+	public class Medal
+	{
+		/// <summary>
+		/// 获取或设置该勋章的标识。
+		/// </summary>
+		[Key]
+		public int Id { get; set; }
+
+		/// <summary>
+		/// 获取或设置该勋章的名称。
+		/// </summary>
+		[Required]
+		[StringLength(100)]
+		public string Name { get; set; } = null!;
+
+		/// <summary>
+		/// 获取或设置该勋章的描述。
+		/// </summary>
+		public string? Description { get; set; }
+
+		/// <summary>
+		/// 获取或设置该勋章对应的图像的路径。
+		/// </summary>
+		[Required]
+		[DataType(DataType.ImageUrl)]
+		public string ImageUri { get; set; } = null!;
+
+		/// <summary>
+		/// 获取或设置单击该勋章时需要跳转的地址。
+		/// </summary>
+		[DataType(DataType.Url)]
+		public string? LinkUri { get; set; }
+
+		/// <summary>
+		/// 获取或设置该勋章关联的类别的标识。
+		/// </summary>
+		public int? CategoryId { get; set; }
+
+		/// <summary>
+		/// 获取或设置该勋章关联的类别。如果为 <c>null</c> 则未关联到任何类别。
+		/// </summary>
+		[ForeignKey(nameof(CategoryId))]
+		public MedalCategory? Category { get; set; }
+	}
+}
---
+++ CC98.Medal/CC98.Medal/Data/MedalCategory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CC98.Medal.Data
+{
+	/// <summary>
+	/// 定义勋章的类别。
+	/// </summary>
+	public class MedalCategory
+	{
+		/// <summary>
+		/// 获取或设置该类别的标识。
+		/// </summary>
+		[Key]
+		public int Id { get; set; }
+
+		/// <summary>
+		/// 获取或设置该类别的上级类别的标识。
+		/// </summary>
+		public int? ParentId { get; set; }
+
+		/// <summary>
+		/// 获取或设置该类别的上级类别。
+		/// </summary>
+		[ForeignKey(nameof(ParentId))]
+		public MedalCategory? Parent { get; set; }
+
+		/// <summary>
+		/// 获取或设置该类别包含的下级类别的集合。
+		/// </summary>
+		[InverseProperty(nameof(Parent))]
+		public IList<MedalCategory> Children { get; set; } = new Collection<MedalCategory>();
+
+		/// <summary>
+		/// 获取或设置该类别包含的勋章的集合。
+		/// </summary>
+		[InverseProperty(nameof(Medal.Category))]
+		public IList<Medal> Medals { get; set; } = new Collection<Medal>();
+	}
+}
